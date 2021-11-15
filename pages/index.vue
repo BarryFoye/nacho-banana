c61922df91214fa2f52be9770bbe2ea7c738702f
Barry Foye <barry.foye@gmail.com>
2021-11-15 20:04:12 +0000

    checkpoint

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="page-wrapper">
-    <div v-for="(d, i) in market_data.data" :key="i">
-      {{ d.id }}
+    <font color="red">
+      <h1>{{ market_data.name }}</h1>
+    </font>
+    <h2>{{ market_data.children.name }}:</h2>
+    <font color="green">
+      <h3>
+        <div>{{ market_data.children.value.sum }}</div>
+      </h3>
+    </font>
+    <div v-for="(d, i) in market_data.children.children" :key="i">
+      {{ i + 1 }}:
+      {{ d.coin_name }}
+      {{ d.quote_market_cap }}
     </div>
   </div>
 </template>
@@ -15,13 +26,25 @@ export default {
     }
   },
   async fetch() {
+    const url = process.env.DATA_SERVICE_HOST || `localhost`
     const market_data = await fetch(
       // `https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/historical?convert=USD,USD,BTC&date=2021-10-24&limit=100&start=1`
-      `http://localhost:3000/v1/market_data/tree_map`
+      `http://${url}:3000/v1/market_data/tree_map`
     ).then((res) => res.json())
 
-    this.market_data = this.market_data.concat(market_data)
-    console.log(this.market_data)
+    this.market_data = market_data
+    console.log('L1**************************************')
+    console.log(market_data.name)
+    console.log('L2**************************************')
+    console.log(market_data.children.name)
+    console.log(market_data.children.value.sum)
+    console.log('L3**************************************')
+    this.market_data.children.children.forEach((element) => {
+      console.log(element.coin_name)
+      console.log(element.quote_market_cap)
+    })
+
+    console.log('****************************************')
   },
 }
 </script>
--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,11 @@
-export const state = () => ({
-  marketDataTreeMap: [],
-})
+// export const state = () => ({
+//   marketDataTreeMap: [],
+// })
 
-export const actions = {
-  FILL_DATA_TREE_MAP({ commit, state }) {
-    commit('SET_TREE_MAP_DATA')
-  },
-}
+// export const actions = {
+//   FILL_DATA_TREE_MAP({ commit, state }) {
+//     commit('SET_TREE_MAP_DATA')
+//   },
+// }
 
-export const mutations = {}
+// export const mutations = {}
