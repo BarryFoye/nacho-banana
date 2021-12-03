a712dfe22e12ce0a7c2601d6c42d4a85fce0b25c
BarryFoye <barry.foye@gmail.com>
2021-12-03 17:18:05 +0000

    closing project

--- store/index.ts
+++ store/index.ts
@@ -2,10 +2,10 @@ export const state = () => ({
   marketDataTreeMap: [],
 })
 
-export const actions = {
-  FILL_DATA_TREE_MAP({ commit, state }) {
-    commit('SET_TREE_MAP_DATA')
-  },
-}
+// export const actions = {
+//   FILL_DATA_TREE_MAP({ commit, state }) {
+//     commit('SET_TREE_MAP_DATA')
+//   },
+// }
 
 export const mutations = {}
